f3314cb0d44cca62c1a8a11b5f33fddcd2ae5781
Ivann Ivann <caliban@mail.bg>
2018-08-26 14:50:07 +0300

    new exercises

---
+++ 2-AppendLists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_AppendLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // създаваме масив от низове, идва от конзолата, разделя се, като се създават подмасиви от низове, в които се махат празните елементи
+            string[] numbersWithSpaces = Console.ReadLine().Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            // обръщат се елементите в масива
+            string[] reversed = numbersWithSpaces.Reverse().ToArray();
+            //създаваме нов лист
+            List<string> numbers = new List<string>();
+            // за всеки елемент от обърнатия масив създаваме масив от низове, разцепени по празно пространство
+            foreach (var item in reversed)
+            {
+                string[] numbersAsText = item.Split();
+
+                foreach (var element in numbersAsText)
+                {
+                    //ако елемента не е празен го добавяме в крайния лист
+                    if (element != "")
+                    {
+                        numbers.Add(element);
+                    }
+                }
+            }
+            //отпечатваме листа, като го набиваме в стринг
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 3-SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3_SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+            // важно е да започва от 1, защото после от индекса вадим 1
+            for (int i = 1; i < input.Count; i++)
+            {
+                if (input[i-1] == input[i])
+                {
+                   
+                    input[i - 1] += input[i];
+                    input.RemoveAt(i);
+                    i = i - 2;
+                    if (i<0)
+                    {
+                        i = 0;
+                    }
+                    
+                    
+                }
+                      
+                
+            }
+            Console.WriteLine(String.Join(" ", input));
+        }
+    }
+}
