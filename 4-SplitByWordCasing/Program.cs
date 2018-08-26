4d0431505d465d784031140a514d7eaf9b4f7abd
Ivann Ivann <caliban@mail.bg>
2018-08-26 22:19:17 +0300

    exercises 4,7

---
+++ 4-SplitByWordCasing/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4_SplitByWordCasing
+{
+    class Program
+    {
+        static string WordType (string word)
+        {
+            var upperLetters = 0;
+            var lowerLetters = 0;
+            string WordType = mixedType;
+
+            foreach (var letter in word)
+            {
+                if (char.IsUpper(letter)
+                {
+                    upperLetters++;
+                }
+                else if (char.IsLower(letter)
+                {
+                    lowerLetters++;
+                }
+                if (upperLetters==word.Length)
+                {
+                    return WordType = UpperCase;
+                }
+                if (lowerLetters==word.Length)
+                {
+                    return WordType = LowerCase;
+                }
+            }
+            return WordType;
+        }
+        static void Main(string[] args)
+        {
+            var separators = new char[]
+               {
+                ',', ';', ':', '.', '!', '(', ')', '"', '\'', '/', '\\', '[',']',' '
+               };
+            var word = Console.ReadLine().Split(separators,
+             StringSplitOptions.RemoveEmptyEntries);//.ToList();
+            var lowerCaseList = new List<string>();
+            var upperCaseList = new List<string>();
+            var mixedCaseList = new List<string>();
+
+            foreach (var w in word)
+            {
+                if (WordType(word[w])="LowerCase")
+                {
+                    lowerCaseList.Add(w);
+                }
+                else if (WordType(word[]) = "UpperCase")
+                {
+                    upperCaseList.Add(w);
+                }
+                else mixedCaseList.Add(w);
+                Console.WriteLine("Lower-case: {0}", string.Join(", ", lowerCaseList));
+                Console.WriteLine("Mixed-case: {0}", string.Join(", ", mixedCaseList));
+                Console.WriteLine("Upper-case: {0}", string.Join(", ", upperCaseList));
+            }
+        }
+        }
+}
---
+++ 7-CountNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7_CountNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //правим масив с размер колкото са самите числа - 1000, тоест индекса е самото число, а съдържанието на индекса е колко пъти се среща числото
+            int[] arr = new int[1001];
+
+            foreach (int num in numbers)
+            {
+                arr[num]++;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]!=0)
+                {
+                    Console.WriteLine("{0} -> {1}",i,arr[i]);
+                }
+            }
+        }
+    }
+}
